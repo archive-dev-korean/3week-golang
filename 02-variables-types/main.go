a6f9f08dd80f7c72ebc6a999a2507c0a9ef6adcc
archive-dev-master <onlybussinessmail@gmail.com>
2026-05-13 16:20:17 +0900

    02 Done
    Signed-off-by: archive-dev-master <onlybussinessmail@gmail.com>

--- 02-variables-types/main.go
+++ 02-variables-types/main.go
@@ -9,11 +9,20 @@ func main() {
 	score := 98.5
 	isStudent := true
 
+	// 여기서부터 수정
+	height := 180
+	email := "alice@example.com"
+	isAdmin := false
+
 	fmt.Println("name:", name)
 	fmt.Println("age:", age)
 	fmt.Println("city:", city)
 	fmt.Println("score:", score)
 	fmt.Println("isStudent:", isStudent)
+	// 여기서부터 수정
+	fmt.Println("height:", height)
+	fmt.Println("email:", email)
+	fmt.Println("isAdmin:", isAdmin)
 
 	age = age + 1
 	fmt.Printf("%s is %d years old next year.\n", name, age)
