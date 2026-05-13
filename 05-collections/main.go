fa2ac7e21cdd19bcc63479bc7686716dc50921df
archive-dev-master <onlybussinessmail@gmail.com>
2026-05-13 17:10:19 +0900

    05 Done
    Signed-off-by: archive-dev-master <onlybussinessmail@gmail.com>

--- 05-collections/main.go
+++ 05-collections/main.go
@@ -8,6 +8,7 @@ func main() {
 	fmt.Println("first number:", numbers[0])
 
 	names := []string{"Alice", "Bob", "Carol"}
+	fmt.Println("orginal slice:", names)
 	names = append(names, "Dave")
 
 	fmt.Println("slice:", names)
@@ -16,15 +17,21 @@ func main() {
 	scores := map[string]int{
 		"Alice": 90,
 		"Bob":   75,
+		"Chanhyuk": 100, //추가됨
 	}
-	scores["Carol"] = 88
 
 	aliceScore, ok := scores["Alice"]
 	if ok {
 		fmt.Println("Alice score:", aliceScore)
 	}
-
+	
 	for name, score := range scores {
 		fmt.Printf("%s: %d점\n", name, score)
 	}
+	//평균 구하기
+	totalScore := 0
+	for _, score := range scores {
+		totalScore += score
+	}
+	fmt.Printf("전체 평균: %d점\n", totalScore / len(scores))
 }
